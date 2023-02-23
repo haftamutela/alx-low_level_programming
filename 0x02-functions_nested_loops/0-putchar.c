d883f73e2e910b8e0daf790db20cd2a52fed0102
haftamutela <haftamuadhena@gmail.com>
2023-02-23 01:03:29 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'print _putchar to stdout'
+ * Return: always o
+ */
+int main (void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putcahr('u');
+	_putcahr('t');
+	_putcahr('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
