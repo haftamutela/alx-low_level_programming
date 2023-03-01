16f4601b89b1aeed7b529b65c164e1ee838b8188
haftamutela <haftamuadhena@gmail.com>
2023-03-01 20:07:48 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,7 @@
-#include <stdio.h>
-<<<<<<< HEAD
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	char c;
-
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-=======
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -24,20 +9,14 @@ int main(void)
  */
 int main(void)
 {
-	int d;
 
-	for (d = 0; d < 100; d++)
+	char ch;
+
+	/* print lowercase letters */
+	for (ch = 'a' ; ch <= 'z' ch++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(ch);
 	}
 	putchar('\n');
-
->>>>>>> 9c65daafeb9727f82dfb22d6e256a5864f060db5
 	return (0);
 }
